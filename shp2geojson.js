aa42bbc383c9aa26d8e1cf40459695b76c7ad5a6
philopian <pdotwdot@gmail.com>
2015-03-24 14:35:50 -0700

    add dependencies

--- shp2geojson.js
+++ shp2geojson.js
@@ -1,3 +1,9 @@
+var fs      = require('fs');
+var shell   = require('shelljs');
+var glob    = require('glob');
+var async   = require('async');
+
+
 function builCmd_shp2wm(IN_FILE,OUT_FILE){  
   return "ogr2ogr -f 'ESRI Shapefile' -t_srs EPSG:4326 "+OUT_FILE+" "+IN_FILE;// EPSG:4326 web mercator
 }
