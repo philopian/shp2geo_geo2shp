1aa8e536afcac39c284699dff91f34abb072128d
philopian <pdotwdot@gmail.com>
2015-03-24 14:46:34 -0700

    geojson2shp: added the logic

--- geojson2shp.js
+++ geojson2shp.js
@@ -11,3 +11,25 @@ function buildCmd_geojson2shp(IN_FILE,OUT_FILE){
   return 'ogr2ogr -f "ESRI Shapefile" '+OUT_FILE+' '+IN_FILE;
 }
 
+
+var workingDir = __dirname+"/data/out/*.geojson";
+glob(workingDir, {}, function (er, files) {
+  if (files.length > 0){
+    for (var i in files){
+      var geojson    = files[i];
+      var shp    = files[i].slice(0, -8).concat(".shp");
+
+      async.series([
+          function(next){ 
+            console.log(".....create shp");
+            var cmd_geo2shp = buildCmd_geojson2shp(geojson,shp);
+            shell.exec(cmd_geo2shp);
+            next();
+          },
+          function(next){
+            console.log("...completed: "+geojson) 
+          }
+      ]);
+    }//for each .shp    
+  }// if *.shp files found
+});
