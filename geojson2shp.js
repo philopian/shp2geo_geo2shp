c74be202c549aa5a15d45a42fad521c182be57c3
philopian <pdotwdot@gmail.com>
2015-03-24 14:49:25 -0700

    geojson2shp: reproject the shp file

--- geojson2shp.js
+++ geojson2shp.js
@@ -12,18 +12,25 @@ function buildCmd_geojson2shp(IN_FILE,OUT_FILE){
 }
 
 
-var workingDir = __dirname+"/data/out/*.geojson";
+var workingDir = __dirname+"/data/*.geojson";
 glob(workingDir, {}, function (er, files) {
   if (files.length > 0){
     for (var i in files){
       var geojson    = files[i];
       var shp    = files[i].slice(0, -8).concat(".shp");
+      var shp_rp_out = files[i].slice(0, -8).concat("_rp.shp");
 
       async.series([
           function(next){ 
             console.log(".....create shp");
             var cmd_geo2shp = buildCmd_geojson2shp(geojson,shp);
             shell.exec(cmd_geo2shp);
+            next();
+          },
+          function(next){
+            console.log(".....reproject shp");
+            var cmd_shp_reproject = builCmd_reprojectShp(shp,shp_rp_out,2991);
+            shell.exec(cmd_shp_reproject);    
             next();
           },
           function(next){
