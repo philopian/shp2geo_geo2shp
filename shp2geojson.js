f46fcabab8cedabad3245925366369dbde6abc5e
philopian <pdotwdot@gmail.com>
2015-04-04 14:11:03 -0700

    re-wrote the shp2geojson so that the temp files are hidden

--- shp2geojson.js
+++ shp2geojson.js
@@ -2,6 +2,7 @@ var fs      = require('fs');
 var shell   = require('shelljs');
 var glob    = require('glob');
 var async   = require('async');
+var path    = require('path');
 
 
 function builCmd_shp2wm(IN_FILE,OUT_FILE){  
@@ -12,31 +13,35 @@ function buildCmd_shp2geojson(IN_FILE,OUT_FILE){
 }
 
 
+
+
 var workingDir = __dirname+"/data/*.shp";
 glob(workingDir, {}, function (er, files) {
   if (files.length > 0){
     for (var i in files){
-      var shp_in    = files[i];
-      var shpWebMer = files[i].slice(0, -4).concat("_rp.shp");
-      var tempfiles = files[i].slice(0, -4).concat("_rp*");
-      var geojson   = files[i].slice(0, -4).concat(".geojson");
+      var shp_in      = files[i];
+      var fileName    = path.basename(shp_in).slice(0,-4);
+      var workingDir  = path.dirname(shp_in);
 
+      var shp_temp_reprj_shp   = path.join(workingDir,"."+fileName).concat(".shp");
+      var shp_temp_reprj_files = path.join(workingDir,"."+fileName).concat(".*");
+      var geojson              = path.join(workingDir,fileName).concat(".geojson");
       async.series([
           function(next){ 
             console.log(".....reproject shp");
-            var cmd_reproject = builCmd_shp2wm(shp_in,shpWebMer);
+            var cmd_reproject = builCmd_shp2wm(shp_in,shp_temp_reprj_shp);
             shell.exec(cmd_reproject);
             next();
           },
           function(next){
             console.log(".....shape to geo");
-            var cmd_shp2geojson = buildCmd_shp2geojson(shpWebMer,geojson);
+            var cmd_shp2geojson = buildCmd_shp2geojson(shp_temp_reprj_shp,geojson);
             shell.exec(cmd_shp2geojson);    
             next();
           },
           function(next){
             console.log(".....delete temp files");
-            var cmd_removeTempFiles = "rm "+tempfiles
+            var cmd_removeTempFiles = "rm "+shp_temp_reprj_files
             shell.exec(cmd_removeTempFiles);    
             next();
           },
@@ -44,7 +49,6 @@ glob(workingDir, {}, function (er, files) {
             console.log("...completed: "+geojson) 
           }
       ]);
-
-    }//for each .shp    
-  }// if *.shp files found
+    }//loop-each-shp-file
+  }
 });
