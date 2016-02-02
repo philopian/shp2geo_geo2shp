cc2e0deed7bc3e5d4c489dbc3853df80baa0bd45
philopian <pdotwdot@gmail.com>
2016-02-02 09:02:20 -0800

    updated the code to be bettergit add .

--- shp2geojson.js
+++ shp2geojson.js
@@ -1,54 +1,26 @@
 var fs      = require('fs');
-var shell   = require('shelljs');
-var glob    = require('glob');
-var async   = require('async');
 var path    = require('path');
+var glob    = require('glob');
+var shell   = require('shelljs');
+// var exec    = require('child_process').exec;
 
 
-function builCmd_shp2wm(IN_FILE,OUT_FILE){  
-  return "ogr2ogr -f 'ESRI Shapefile' -t_srs EPSG:4326 "+OUT_FILE+" "+IN_FILE;// EPSG:4326 web mercator
+function createGeojsonFileName(inFileName){
+  return inFileName.slice(0, -4)+'.geojson';
 }
-function buildCmd_shp2geojson(IN_FILE,OUT_FILE){
-  return 'ogr2ogr -f "GeoJSON" '+OUT_FILE+' '+IN_FILE;
+function cmdShp2Geojson(IN_FILE,OUT_FILE){
+  return "ogr2ogr -f 'GeoJSON' -t_srs EPSG:4326 "+OUT_FILE+" "+IN_FILE;// EPSG:4326 web mercator
 }
 
 
-
-
-var workingDir = __dirname+"/data/*.shp";
-glob(workingDir, {}, function (er, files) {
-  if (files.length > 0){
-    for (var i in files){
-      var shp_in      = files[i];
-      var fileName    = path.basename(shp_in).slice(0,-4);
-      var workingDir  = path.dirname(shp_in);
-
-      var shp_temp_reprj_shp   = path.join(workingDir,"."+fileName).concat(".shp");
-      var shp_temp_reprj_files = path.join(workingDir,"."+fileName).concat(".*");
-      var geojson              = path.join(workingDir,fileName).concat(".geojson");
-      async.series([
-          function(next){ 
-            console.log(".....reproject shp");
-            var cmd_reproject = builCmd_shp2wm(shp_in,shp_temp_reprj_shp);
-            shell.exec(cmd_reproject);
-            next();
-          },
-          function(next){
-            console.log(".....shape to geo");
-            var cmd_shp2geojson = buildCmd_shp2geojson(shp_temp_reprj_shp,geojson);
-            shell.exec(cmd_shp2geojson);    
-            next();
-          },
-          function(next){
-            console.log(".....delete temp files");
-            var cmd_removeTempFiles = "rm "+shp_temp_reprj_files
-            shell.exec(cmd_removeTempFiles);    
-            next();
-          },
-          function(next){
-            console.log("...completed: "+geojson) 
-          }
-      ]);
-    }//loop-each-shp-file
-  }// if *.shp files found
+var workingDir = "./data/**/*.shp";
+glob(workingDir, {}, function (err, files) {
+  if (err) throw err;
+  for (var i in files) {
+    var inFile = files[i];
+    var outFile = createGeojsonFileName(files[i]);
+    var cmd = cmdShp2Geojson(inFile,outFile);
+    console.log("....making..(%s)",outFile);
+    shell.exec(cmd);
+  }
 });
